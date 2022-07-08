0e18e6088cecd21c1d9261837e8440f8bcc40ae8
Rakan Alshammari <i.rakkan66@gmail.com>
2022-07-08 17:19:45 +0300

    add world map

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,6 +1,17 @@
 <template>
- <div>asd</div>
-
+  <div>
+    <div class="bg-gray-300">
+      <WorldMap></WorldMap>
+    </div>
+    <div class="p-8 max-w-lg mx-auto mt-12">
+      <button
+        type="button"
+        class="inline-flex justify-center items-center bg-green-600 hover:bg-green-700 hover:border-green-700 rounded text-white border-green-600 font-semibold shadow focus:outline-none py-3 px-5 w-full"
+      >
+        عرض قائمة الدول
+      </button>
+    </div>
+  </div>
 </template>
 
 <script>
