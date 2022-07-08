43ab1814525ed53672b732f72f468fb24bf8c4d1
Rakan Alshammari <i.rakkan66@gmail.com>
2022-07-09 00:54:52 +0300

    add covid factory

---
+++ api/database/factories/CovidFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Covid>
+ */
+class CovidFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'country' => $this->faker->country,
+            'total_confirmed' => $this->faker->numberBetween(0, 1000000),
+            'total_recovered' => $this->faker->numberBetween(0, 1000000),
+            'total_deaths' => $this->faker->numberBetween(0, 1000000),
+        ];
+    }
+}
