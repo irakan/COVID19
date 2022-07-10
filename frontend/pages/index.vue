d9b998f1e5919086dc03715203af84787ff0e2a3
Rakan Alshammari <i.rakkan66@gmail.com>
2022-07-10 05:42:07 +0300

    add link

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -9,12 +9,12 @@
       <div v-else class="svgMap-map-empty-div"></div>
     </div>
     <div class="p-8 max-w-lg mx-auto mt-12">
-      <button
-        type="button"
+      <NuxtLink
+        to="/countries"
         class="inline-flex justify-center items-center bg-green-600 hover:bg-green-700 hover:border-green-700 rounded text-white border-green-600 font-semibold shadow focus:outline-none py-3 px-5 w-full"
       >
         عرض قائمة الدول
-      </button>
+      </NuxtLink>
     </div>
   </div>
 </template>
