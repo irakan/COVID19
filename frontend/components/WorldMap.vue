d43066078f7ccb926dea0334ee27c0e21d4d9bdf
Rakan Alshammari <i.rakkan66@gmail.com>
2022-07-10 05:15:47 +0300

    add api data to the map

--- frontend/components/WorldMap.vue
+++ frontend/components/WorldMap.vue
@@ -5,9 +5,18 @@
 <script>
 import svgMap from "svgmap";
 import "svgmap/dist/svgMap.min.css";
-import countriesAR from "@/data/countriesAR";
 
 export default {
+  props: {
+    mapDataOptions: {
+      type: Object,
+      default: () => ({}),
+    },
+    customTooltipFunction: {
+      type: Function,
+      default: () => ({}),
+    },
+  },
   data() {
     return {
       id: "svgMap",
@@ -15,71 +24,23 @@ export default {
     };
   },
   methods: {
-    customTooltip(tooltipDiv, countryID, countryValues) {
-      let countryName = countriesAR[countryID];
-
-      // Start countainer
-      var innerHTML =
-        "<div class='flex flex-col justify-center items-center w-96 shadow bg-white py-2 px-3 rounded-lg border-white text-gray-600'>";
-
-      // Country name
-      innerHTML +=
-        "<div class='flex justify-center text-lg border-b w-full py-2'>" +
-        countryName +
-        "</div>";
-
-      // Country values
-      if (countryValues) {
-        innerHTML +=
-          "<div class='flex justify-between items-center w-full py-2 text-sm'><div>مجموع حالات كورونا المؤكدة (Total Confirmed)</div><div>" +
-          (countryValues.totalConfirmed ?? "غير معروف") +
-          "</div></div>";
-        innerHTML +=
-          "<div class='flex justify-between items-center w-full py-2 text-sm'><div>مجموع حالات التعافي (Total Recovered)</div><div>" +
-          (countryValues.totalRecovered ?? "غير معروف") +
-          "</div></div>";
-        innerHTML +=
-          "<div class='flex justify-between items-center w-full py-2 text-sm'><div>مجموع الوفيات (Total Deaths)</div><div>" +
-          (countryValues.totalDeaths ?? "غير معروف") +
-          "</div></div>";
-      } else {
-        innerHTML +=
-          "<div class='w-full py-2 text-center'><div>لا توجد بيانات.</div></div>";
-      }
-
-      // End countainer
-      innerHTML += "</div>";
-
-      tooltipDiv.innerHTML = innerHTML;
+    initMap() {
+      let self = this;
+      this.map = new svgMap({
+        targetElementID: this.id,
+        data: { ...this.mapDataOptions },
+        onGetTooltip: function (tooltipDiv, countryID, countryValues) {
+          return self.customTooltipFunction(
+            tooltipDiv,
+            countryID,
+            countryValues
+          );
+        },
+      });
     },
   },
   mounted() {
-    let self = this;
-    this.map = new svgMap({
-      targetElementID: this.id,
-      data: {
-        data: {
-          totalConfirmed: {
-            name: "الحالات المؤكدة (Total Confirmed):",
-          },
-          totalRecovered: {
-            name: "الحالات المؤكدة (Total Confirmed):",
-          },
-          totalDeaths: {
-            name: "الحالات المؤكدة (Total Confirmed):",
-          },
-        },
-        applyData: "totalConfirmed",
-        values: {
-          AF: { totalConfirmed: 587, totalRecovered: 300 },
-          AL: { totalConfirmed: 44, totalRecovered: 2, totalDeaths: 30 },
-          DZ: { totalConfirmed: 100, totalRecovered: 4, totalDeaths: 60 },
-        },
-      },
-      onGetTooltip: function (tooltipDiv, countryID, countryValues) {
-        return self.customTooltip(tooltipDiv, countryID, countryValues);
-      },
-    });
+    this.initMap();
   },
 };
 </script>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,7 +1,12 @@
 <template>
   <div>
     <div class="bg-gray-300">
-      <WorldMap></WorldMap>
+      <WorldMap
+        v-if="mapDataOptions"
+        :map-data-options="mapDataOptions"
+        :custom-tooltip-function="customTooltip"
+      ></WorldMap>
+      <div v-else style="padding-top: 30%"></div>
     </div>
     <div class="p-8 max-w-lg mx-auto mt-12">
       <button
@@ -15,7 +20,85 @@
 </template>
 
 <script>
+import countriesAR from "@/data/countriesAR";
+
 export default {
-  name: "IndexPage",
+  data() {
+    return {
+      countriesSummary: {},
+      mapDataOptions: null,
+    };
+  },
+  methods: {
+    setWorldMapData() {
+      this.mapDataOptions = {
+        data: {
+          totalConfirmed: {
+            name: "الحالات المؤكدة (Total Confirmed):",
+          },
+          totalRecovered: {
+            name: "الحالات المؤكدة (Total Confirmed):",
+          },
+          totalDeaths: {
+            name: "الحالات المؤكدة (Total Confirmed):",
+          },
+        },
+        applyData: "totalConfirmed",
+        values: this.countriesSummary,
+      };
+    },
+    customTooltip(tooltipDiv, countryID, countryValues) {
+      let countryName = countriesAR[countryID];
+
+      // Start countainer
+      var innerHTML =
+        "<div class='flex flex-col justify-center items-center w-96 shadow bg-white py-2 px-3 rounded-lg border-white text-gray-600'>";
+
+      // Country name
+      innerHTML +=
+        "<div class='flex justify-center text-lg border-b w-full py-2'>" +
+        countryName +
+        "</div>";
+
+      // Country values
+      if (countryValues) {
+        innerHTML +=
+          "<div class='flex justify-between items-center w-full py-2 text-sm'><div>مجموع حالات كورونا المؤكدة (Total Confirmed)</div><div>" +
+          (countryValues.totalConfirmed.toLocaleString("en") ?? "غير معروف") +
+          "</div></div>";
+        innerHTML +=
+          "<div class='flex justify-between items-center w-full py-2 text-sm'><div>مجموع حالات التعافي (Total Recovered)</div><div>" +
+          (countryValues.totalRecovered.toLocaleString("en") ?? "غير معروف") +
+          "</div></div>";
+        innerHTML +=
+          "<div class='flex justify-between items-center w-full py-2 text-sm'><div>مجموع الوفيات (Total Deaths)</div><div>" +
+          (countryValues.totalDeaths.toLocaleString("en") ?? "غير معروف") +
+          "</div></div>";
+      } else {
+        innerHTML +=
+          "<div class='w-full py-2 text-center'><div>لا توجد بيانات.</div></div>";
+      }
+
+      // End countainer
+      innerHTML += "</div>";
+
+      tooltipDiv.innerHTML = innerHTML;
+    },
+  },
+  async mounted() {
+    let self = this;
+    const response = await this.$api.countries.summary();
+
+    response.data.map((country) => {
+      self.countriesSummary[country.code] = {
+        code: country.code,
+        name: country.name,
+        totalConfirmed: country.totalConfirmed,
+        totalRecovered: country.totalRecovered,
+        totalDeaths: country.totalDeaths,
+      };
+    });
+    this.setWorldMapData();
+  },
 };
 </script>
