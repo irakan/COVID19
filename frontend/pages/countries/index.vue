dc6dbd776ea6ae09b2f23052f86b37814384c25b
Rakan Alshammari <i.rakkan66@gmail.com>
2022-07-11 05:33:06 +0300

    add spinner while trying to fill data

--- frontend/pages/countries/index.vue
+++ frontend/pages/countries/index.vue
@@ -13,10 +13,31 @@
       >
       <a
         href="#"
-        class="inline-flex justify-center items-center bg-green-600 hover:bg-green-700 hover:border-green-700 rounded text-white border-green-600 font-semibold shadow focus:outline-none py-3 px-5"
+        class="flex justify-center items-center bg-green-600 hover:bg-green-700 hover:border-green-700 rounded text-white border-green-600 font-semibold shadow focus:outline-none py-3 px-5"
         @click.prevent="fillData"
-        >تعبئة البيانات</a
       >
+        <span v-if="!fillingData">تعبئة البيانات</span>
+        <svg
+          v-else
+          class="animate-spin h-6 w-5 text-white"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <circle
+            class="opacity-25"
+            cx="12"
+            cy="12"
+            r="10"
+            stroke="currentColor"
+            stroke-width="4"
+          />
+          <path
+            class="opacity-75"
+            fill="currentColor"
+            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+          /></svg
+      ></a>
     </div>
     <div class="lg:flex h-full">
       <aside class="flex flex-col lg:w-3/12 bg-white rounded-lg shadow">
@@ -131,6 +152,7 @@ export default {
   data() {
     return {
       countries: null,
+      fillingData: false,
     };
   },
   async mounted() {
@@ -138,12 +160,15 @@ export default {
   },
   methods: {
     async fillData() {
+      this.fillingData = true;
       try {
         this.$toast.show("جاري تعبئة البيانات...");
         await this.$api.countries.fill();
         this.$toast.success("تم تعبئة البيانات  بنجاح.");
       } catch (e) {
         this.$toast.error(e.message);
+      } finally {
+        this.fillingData = false;
       }
     },
     async loadPageCountries(page) {
