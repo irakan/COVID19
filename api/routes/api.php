c13c01d2ccdcf262ba1fd181820559d2316e8b42
Rakan Alshammari <i.rakkan66@gmail.com>
2022-07-12 08:09:47 +0300

    update

--- api/routes/api.php
+++ api/routes/api.php
@@ -6,6 +6,9 @@ use App\Http\Controllers\SummaryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::get('countries', [CountryController::class, 'index'])->name('countries.index');
+Route::get('countries/summary', SummaryController::class)->name('countries.summary');
+
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
@@ -17,6 +20,3 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::get('countries/{country}', [CountryController::class, 'show'])->name('countries.show');
     Route::patch('countries/{country}', [CountryController::class, 'update'])->name('countries.update');
 });
-
-Route::get('countries', [CountryController::class, 'index'])->name('countries.index');
-Route::get('countries/summary', SummaryController::class)->name('countries.summary');
